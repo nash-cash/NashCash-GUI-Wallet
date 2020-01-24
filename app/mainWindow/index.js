d1509f6ebb20cc8fdf0fd8b61cf9451d530e4c56
LeinAd2k <45920023+Nash-Cash@users.noreply.github.com>
2020-01-24 12:01:04 +0100

    Update index.js

--- app/mainWindow/index.js
+++ app/mainWindow/index.js
@@ -38,8 +38,8 @@ export function savedInInstallDir(savePath: string) {
 const homedir = os.homedir();
 
 export const directories = [
-  `${homedir}/.nashguiwallet`,
-  `${homedir}/.nashguiwallet/logs`
+  `${homedir}/.protonwallet`,
+  `${homedir}/.protonwallet/logs`
 ];
 
 export const il8n = new LocalizedStrings({
